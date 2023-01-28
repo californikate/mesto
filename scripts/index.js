f4563f3058be580de96d27fb4d29f61219b3523b
Katerina Nekrasova <regina.phalange8@yandex.ru>
2023-01-28 19:59:47 +0500

    feat: настроить отправку формы

--- scripts/index.js
+++ scripts/index.js
@@ -1,26 +1,44 @@
-const profileEditButton = document.querySelector('.profile__edit-button');
+const editButton = document.querySelector('.profile__edit-button');
 const popup = document.querySelector('.popup');
 const closeButton = document.querySelector('.popup__close-button');
 
+const formElement = document.querySelector('#popup__form_element');
+const nameInput = document.querySelector('.popup__form_name');
+const jobInput = document.querySelector('.popup__form_job');
+
+const profileTitle = document.querySelector('.profile__title');
+const profileSubtitle = document.querySelector('.profile__subtitle');
 
 function toggleOpenPopup() {
   popup.classList.toggle('popup_opened');
-}
+};
 
-function handleProfileEditButtonClick() {
+function handleEditButtonClick() {
   toggleOpenPopup();
-}
+  nameInput.value = profileTitle.textContent;
+  jobInput.value = profileSubtitle.textContent;
+};
 
 function handleCloseButtonClick() {
   toggleOpenPopup();
-}
+};
 
-function handleOverlayClick(event) {
-  if (event.target === event.currentTarget) {
-    toggleOpenPopup();
-  }
-}
+//function handleOverlayClick(event) {
+//  if (event.target === event.currentTarget) {
+//    toggleOpenPopup();
+//  }
+//};
 
-profileEditButton.addEventListener('click', handleProfileEditButtonClick);
+function handleFormSubmit (evt) {
+  evt.preventDefault();
+  profileTitle.textContent = nameInput.value;
+  profileSubtitle.textContent = jobInput.value;
+  handleCloseButtonClick();
+  console.log('hello');
+};
+
+editButton.addEventListener('click', handleEditButtonClick);
 closeButton.addEventListener('click', handleCloseButtonClick);
 popup.addEventListener('click', handleOverlayClick);
+
+formElement.addEventListener('submit', handleFormSubmit); 
