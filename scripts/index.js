1de0d166f3bf295e477019eac9343ef6dc745463
Katerina Nekrasova <regina.phalange8@yandex.ru>
2023-01-28 14:52:15 +0500

    feat: подключить открытие и закрытие попапа

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,26 @@
-const profileAddButton = document.querySelector('.');
+const profileEditButton = document.querySelector('.profile__edit-button');
+const popup = document.querySelector('.popup');
+const closeButton = document.querySelector('.popup__close-button');
+
+
+function toggleOpenPopup() {
+  popup.classList.toggle('popup_opened');
+}
+
+function handleProfileEditButtonClick() {
+  toggleOpenPopup();
+}
+
+function handleCloseButtonClick() {
+  toggleOpenPopup();
+}
+
+function handleOverlayClick(event) {
+  if (event.target === event.currentTarget) {
+    toggleOpenPopup();
+  }
+}
+
+profileEditButton.addEventListener('click', handleProfileEditButtonClick);
+closeButton.addEventListener('click', handleCloseButtonClick);
+popup.addEventListener('click', handleOverlayClick);
