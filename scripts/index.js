2ff039c93fda03717fd34a70dd6ebdef86de095b
Katerina Nekrasova <regina.phalange8@yandex.ru>
2023-02-10 17:44:14 +0500

    feat: template

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,11 @@
 // Открытие и закрытие модального окна "Редактировать профиль"
 // определяем переменные для кнопок и попапа
-
 const editButton = document.querySelector('.profile__edit-button');
+const addButton = document.querySelector('.profile__add-button');
 const popup = document.querySelector('.popup');
 const closeButton = document.querySelector('.popup__close-button');
 
 // определяем переменные для значения полей "Имя" и "О себе"
-
 const nameInput = document.querySelector('.popup__input_type_name');
 const jobInput = document.querySelector('.popup__input_type_job');
 
@@ -14,12 +13,23 @@ const profileTitle = document.querySelector('.profile__title');
 const profileSubtitle = document.querySelector('.profile__subtitle');
 
 //определяем переменную для формы отправки данных
-
 const formElement = document.querySelector('.popup__form-element');
+
+// определяем переменные для template
+const cardTemplate = document.querySelector('#card-template').content;
+const cardsOnline = document.querySelector('.elements__items');
+
+// определяем переменную для кнопки лайка
+
 
 // toggle добавления/удаления класса для открытия/закрытия попапа
 function toggleOpenPopup() {
   popup.classList.toggle('popup_opened');
+};
+
+//функция открытия попапа по клику на кнопку "Добавить"
+function handleAddButtonClick() {
+  toggleOpenPopup();
 };
 
 // функция открытия попапа по клику на кнопку "Редактировать"
@@ -38,7 +48,6 @@ function handleEditButtonClick() {
 }; */
 
 // Специальное событие submit отправляет форму
-
 function handleFormSubmit (evt) {
   evt.preventDefault();
   profileTitle.textContent = nameInput.value;
@@ -49,8 +58,47 @@ function handleFormSubmit (evt) {
 
 // Прикрепляем обработчики к формам, которые будут следить за событиями
 
+addButton.addEventListener('click', handleAddButtonClick); // кнопка Добавить
 editButton.addEventListener('click', handleEditButtonClick); // кнопка Редактировать
 closeButton.addEventListener('click', toggleOpenPopup); // кнопка закрыть попап
 // popup.addEventListener('click', handleOverlayClick); // клик на оверлэй
 
 formElement.addEventListener('submit', handleFormSubmit); // отправка новых данных
+
+
+
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+//копируем и наполяем содержимым тег template
+initialCards.forEach((element) => {
+  const cardContainer = cardTemplate.querySelector('.element').cloneNode(true);
+  cardContainer.querySelector('.element__photo').src = element.link;
+  cardContainer.querySelector('.element__title').textContent = element.name;
+  // отображаем на странице 
+  cardsOnline.append(cardContainer);
+});
