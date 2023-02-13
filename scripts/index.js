a01ea8a5dfb0a0a23e36f5dd19e84dcf189f649e
Katerina Nekrasova <regina.phalange8@yandex.ru>
2023-02-13 18:20:45 +0500

    fix: close button

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,8 @@
-// определяем переменные для кнопок и попапа
+// определяем переменные для кнопок открытия попапов
 const editButton = document.querySelector('.profile__edit-button');
 const addButton = document.querySelector('.profile__add-button');
-const closeButton = document.querySelector('.popup__close-button');
+
+// определяем переменные для попапов
 const popup = document.querySelector('.popup');
 const editPopup = document.querySelector('.edit-popup');
 const addPopup = document.querySelector('.add-popup');
@@ -25,6 +26,12 @@ const addProfileForm = document.querySelector('#add-profile');
 // определяем переменные для template
 const cardTemplate = document.querySelector('#card-template').content;
 const cardsList = document.querySelector('.elements__items');
+
+// определяем переменные для кнопок закрытия попапов
+const editPopupClose = editPopup.querySelector('.popup__close-button');
+const addProfileClose = addPopup.querySelector('.popup__close-button');
+const imagePopupClose = imagePopup.querySelector('.popup__close-button');
+
 
 // массив данных для карточек
 const initialCards = [
@@ -54,48 +61,36 @@ const initialCards = [
   }
 ];
 
-// починить кнопку закрытия на попапе Добавить и Картинка, починить попап Добавить
 // функции добавления/удаления класса для открытия/закрытия любого попапа
-function openPopup(popup) {
-  popup.classList.add('popup_opened');
-};
-
-function closePopup() {
-  popup.classList.remove('popup_opened');
+function toggleOpenPopup(popup) {
+  popup.classList.toggle('popup_opened');
 };
 
 // функция открытия попапа по клику на кнопку "Редактировать"
 function handleEditButtonClick() {
-  openPopup(editPopup);
+  toggleOpenPopup(editPopup);
   // При открытии формы поля "Имя" и "О себе" заполнены теми значениями, которые отображаются на странице
   nameInput.value = profileTitle.textContent;
   jobInput.value = profileSubtitle.textContent;
 };
 
-// Специальное событие submit отправляет форму попапа Редактировать
+// Специальное событие submit отправляет форму попапа "Редактировать"
 function handleFormSubmit (evt) {
   evt.preventDefault();
   profileTitle.textContent = nameInput.value;
   profileSubtitle.textContent = jobInput.value;
   // закрытие формы после отправки
-  closePopup(editPopup);
+  toggleOpenPopup(editPopup);
 };
 
-//функция открытия попапа по клику на кнопку "Добавить"
-// УБРАТЬ В ОБРАБОТЧИКИ СОБЫТИЙ? или добавить при открытии пустые окна. 
-//они по идее должны быть автоматически пустыми, если бы у меня работал этот попап
-function handleAddButtonClick() {
-  openPopup(addPopup);
-};
-
-// Специальное событие submit отправляет форму попапа Добавить
+// Специальное событие submit отправляет форму попапа "Добавить"
 function handleFormCreate (evt) {
   evt.preventDefault();
   const element = createElement(placeNameInput.value, placeUrlInput.value);
   cardsList.prepend(element);
   evt.target.reset();
   // закрытие формы после отправки
-  closePopup(addPopup);
+  toggleOpenPopup(addPopup);
 };
 
 // дополнительно закрытие окна по клику на оверлэй - необходимо отредактировать
@@ -115,7 +110,7 @@ function createElement(name, link) {
   const cardTitle = cardElement.querySelector('.element__title');
   cardTitle.textContent = name;
 
-  // нажатие кнопки лайка
+  // нажатие кнопки "Лайк"
   const likeButton = cardElement.querySelector('.element__like-button');
   likeButton.addEventListener('click', function(evt) {
     evt.target.classList.toggle('element__like-button_active');
@@ -131,7 +126,7 @@ function createElement(name, link) {
   const popupImage = document.querySelector('.popup__image');
   const popupCaption = document.querySelector('.popup__caption');
   cardImage.addEventListener('click', function(evt) {
-    openPopup(imagePopup);
+    toggleOpenPopup(imagePopup);
     // добавляем значения для картинок и названий
     popupImage.src = link;
     popupImage.alt = name;
@@ -149,10 +144,29 @@ initialCards.forEach(function (card) {
 
 
 // Прикрепляем обработчики к формам, которые будут следить за событиями
-editButton.addEventListener('click', handleEditButtonClick); // кнопка Редактировать
-addButton.addEventListener('click', handleAddButtonClick); // кнопка Добавить
-closeButton.addEventListener('click', closePopup); // кнопка закрыть любой попап
-// popup.addEventListener('click', handleOverlayClick); // клик на оверлэй
+editButton.addEventListener('click', handleEditButtonClick); // кнопка "Редактировать"
+
+// функция открытия попапа по клику на кнопку "Добавить"
+addButton.addEventListener('click', function() {
+  toggleOpenPopup(addPopup)
+});
 
 editProfileForm.addEventListener('submit', handleFormSubmit); // отправка новых данных
 addProfileForm.addEventListener('submit', handleFormCreate); // создание новой карточки
+
+// закрыть форму редактирование профиля
+editPopupClose.addEventListener('click', function() {
+  toggleOpenPopup(editPopup)
+});
+
+// закрыть форму добавления
+addProfileClose.addEventListener('click', function () {
+  toggleOpenPopup(addPopup)
+});
+
+// закрыть попас с картинкой
+imagePopupClose.addEventListener('click',function () {
+  toggleOpenPopup(imagePopup)
+});
+
+// popup.addEventListener('click', handleOverlayClick); // клик на оверлэй
